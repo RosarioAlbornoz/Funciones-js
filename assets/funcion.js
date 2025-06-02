690807c727e48ee5b5a5bb19e4da60558e5bfe39
rosarioalbornoz <rosarioalbornoz@live.com.ar>
2025-06-02 17:04:24 -0300

    Listo

---
+++ assets/funcion.js
@@ -0,0 +1,7 @@
+alert ("Si tenés otra duda,  no dudes en contactarnos!")
+
+
+let ciudad = prompt("Si te gustaria tener un Catfecito cerca,  contanos cual es tu ciudad")
+
+console.log(ciudad);
+
--- funcion.js
+++
@@ -1,6 +0,0 @@
-alert ("Si tenes otra duda que aqui no se te despeje no dudes en contactarnos!")
-
-
-let ciudad = prompt("Si te gustaria tener un Catfecito cerca contanos cual es tu ciudad")
-
-
